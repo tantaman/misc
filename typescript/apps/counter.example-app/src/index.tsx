6eb9adc67b1aa8ef93c8cbfda5c9df9fc6a2c5a9
tantaman <matt.wonlaw@gmail.com>
2022-01-07 22:15:19 -0500

    sketch out the example app which will use counters and launch the counter ui

---
+++ typescript/apps/counter.example-app/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import counterUiLauncher from "@strut/counter.ui.launcher";
+import counter from "@strut/counter";
+
+function App() {
+  return <div>Welcome to example!</div>
+}
+
+render(<App />, document.getElementById('content'));
