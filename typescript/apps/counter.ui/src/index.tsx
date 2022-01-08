37b657aba706ac857a21b6c5c22480f62f26f825
tantaman <matt.wonlaw@gmail.com>
2022-01-07 21:54:11 -0500

    package.json for all the sub-packages

--- typescript/apps/counter.ui/src/index.tsx
+++ typescript/apps/counter.ui/src/index.tsx
@@ -4,7 +4,36 @@ import { render } from "react-dom";
 const el = document.getElementById('counter-ui-root');
 
 function App() {
-  return <div></div>;
+  return <div>Welcome to Counter UI!</div>;
 }
 
 render(<App />, el);
+
+// Grab the window id
+// post to our parent that we're alive
+// wait for catchup data
+// start rendering
+
+const searchParams = new URL(location.href).searchParams;
+const myId = searchParams.get('window-id');
+const openerId = searchParams.get('opener-id');
+
+window.addEventListener("message", (e) => {
+  if (e.data.source !== openerId) {
+    return;
+  }
+
+  switch (e.data.event) {
+    case "catchup":
+      break;
+    case "update":
+      break;
+  }
+});
+
+if (window.opener != null) {
+  window.opener.postMessage({
+    source: myId,
+    even: "ready",
+  }, "*");
+}
