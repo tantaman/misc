b2a0a175e27d4c297d80bcebdb7b6776c01c24a2
tantaman <matt.wonlaw@gmail.com>
2022-01-11 21:24:05 -0500

    memoization concepts to simplify gen methods in models
    e.g., Slide->genDrawing

---
+++ typescript/packages/utils/src/memoize.ts
@@ -0,0 +1,35 @@
+export interface MemoizedFn<T> {
+  (): T;
+  clear: () => void;
+  set: (T) => void;
+  get: () => T | undefined;
+}
+
+export default function memoize<T>(fn: () => T): MemoizedFn<T> {
+  let cached: T | undefined;
+  let run = false;
+  const ret = () => {
+    if (!run) {
+      // re-entrancy would be a problem in that we'll execute more than
+      // once.
+      cached = fn();
+      run = true;
+    }
+
+    return cached;
+  };
+
+  ret.clear = () => {
+    run = false;
+    cached = undefined;
+  };
+
+  ret.set = (x: T) => {
+    run = true;
+    cached = x;
+  };
+
+  ret.get = () => cached;
+
+  return ret;
+}
