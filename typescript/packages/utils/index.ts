d9184588821e16f4ac17b7bf864b19767c9202d4
Matt Crinklaw-Vogt <mlaw@fb.com>
2022-01-18 08:55:58 -0500

    export memoize

--- typescript/packages/utils/index.ts
+++ typescript/packages/utils/index.ts
@@ -3,5 +3,6 @@ import * as math from './src/math';
 import notEmpty from './src/notEmpty';
 import nullthrows from './src/nullthrows';
 import typedKeys from './src/typedKeys_UNSAFE';
+import memoize from './src/memoize';
 
-export { invariant, math, notEmpty, nullthrows, typedKeys };
+export { invariant, math, notEmpty, nullthrows, typedKeys, memoize };
