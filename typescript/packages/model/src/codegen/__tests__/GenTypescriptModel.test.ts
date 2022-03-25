97ccdc0e40c71d6021304d53bae1aea6b7bec2c1
Matt <matt.wonlaw@gmail.com>
2022-03-25 09:28:27 -0400

    fixup codegen tests

--- typescript/packages/model/src/codegen/__tests__/GenTypescriptModel.test.ts
+++ typescript/packages/model/src/codegen/__tests__/GenTypescriptModel.test.ts
@@ -24,100 +24,86 @@ class PrimitiveFieldsSchema extends Schema {
 }
 
 test("Generating an ID only model", () => {
+  // console.log(genIt(IDOnlySchema.get()).contents);
   expect(genIt(IDOnlySchema.get()).contents).toEqual(
-    `import Model from '@strut/model/Model.js';
-import {SID_of} from '@strut/sid';
-
+    `// SIGNED-SOURCE: <0806e7cddeb1155062bad7ac7e9e913f>
+import Model, { Spec } from "@strut/model/Model.js";
+import { SID_of } from "@strut/sid";
 
 export type Data = {
-  id: SID_of<any>
+  id: SID_of<any>;
 };
 
-
-export default class IDOnly
-  extends Model<Data> {
-    
+export default class IDOnly extends Model<Data> {
   get id(): SID_of<any> {
     return this.data.id;
   }
-
-  
 }
 
-
-export const spec = {
+export const spec: Spec<Data> = {
   createFrom(data: Data) {
     return new IDOnly(data);
   },
 
-  nativeStorageType: "MySQL",
-}
-
+  storageDescriptor: {
+    nativeStorageType: "Postgres",
+  },
+};
 `
   );
 });
 
 test("Generating all primitive fields", () => {
-  console.log(genIt(PrimitiveFieldsSchema.get()).contents);
+  // console.log(genIt(PrimitiveFieldsSchema.get()).contents);
   expect(genIt(PrimitiveFieldsSchema.get()).contents)
-    .toEqual(`import Model from '@strut/model/Model.js';
-import {SID_of} from '@strut/sid';
-
+    .toEqual(`// SIGNED-SOURCE: <49fd8661bff6f3ce5bf2766ebfb9018b>
+import Model, { Spec } from "@strut/model/Model.js";
+import { SID_of } from "@strut/sid";
 
 export type Data = {
-  mrBool: boolean,
-  mrString: string,
-  mrInt32: number,
-  mrInt64: string,
-  mrUint64: string,
-  mrFloat: number
+  mrBool: boolean;
+  mrString: string;
+  mrInt32: number;
+  mrInt64: string;
+  mrUint64: string;
+  mrFloat: number;
 };
 
-
-export default class PrimitiveFields
-  extends Model<Data> {
-    
+export default class PrimitiveFields extends Model<Data> {
   get mrBool(): boolean {
     return this.data.mrBool;
   }
 
-  
   get mrString(): string {
     return this.data.mrString;
   }
 
-  
   get mrInt32(): number {
     return this.data.mrInt32;
   }
 
-  
   get mrInt64(): string {
     return this.data.mrInt64;
   }
 
-  
   get mrUint64(): string {
     return this.data.mrUint64;
   }
 
-  
   get mrFloat(): number {
     return this.data.mrFloat;
   }
-
-  
 }
 
-
-export const spec = {
+export const spec: Spec<Data> = {
   createFrom(data: Data) {
     return new PrimitiveFields(data);
   },
 
-  nativeStorageType: "MySQL",
-}
-
+  storageDescriptor: {
+    nativeStorageType: "Postgres",
+  },
+};
 `);
 });
 
